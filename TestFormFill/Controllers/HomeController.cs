eb37793ebfbf8ebfc833ca51d5758019e8a5060b
Darren Gallagher <darren@darrengallagher.co.uk>
2012-11-14 22:35:40 +0000

    zzzzz

---
+++ TestFormFill/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace TestFormFill.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            WebRequest request = HttpWebRequest.Create("http://www.tredz.co.uk");
+
+            WebResponse response = request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string urlText = reader.ReadToEnd();
+
+
+            return View();
+        }
+    }
+}
